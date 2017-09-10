6479bcb250a8446b4a3b6a5ad563ca63702d56f7
ignaszienius <30698624+ignaszienius@users.noreply.github.com>
2017-09-10 21:43:46 +0300

    paruosta aplinka

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vaivorykštė</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<body>
+	<div class="container">
+		<div class="row bg-warning">	
+						<h1>Vaivorykštė</h1>
+		</div>			
+		<div class="row bg-success">
+
+				<i class="fa fa-sun-o" aria-hidden="true"></i>
+
+		</div>	
+	</div>	
+</body>
+</html>
